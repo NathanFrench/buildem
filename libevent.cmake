e0ad8bb450fd5d03bc558ce57d43f34cb0742d44
Nathan French <nate@cl0d.com>
2017-06-21 08:39:18 -0700

    added libevent build

---
+++ libevent.cmake
@@ -0,0 +1,27 @@
+set (LIBEVENT_UPDATE_COMMAND git checkout master && git pull)
+
+
+include (ExternalProject)
+include (ExternalSource)
+include (BuildSupport)
+
+external_git_repo (libevent
+    HEAD
+    http://www.github.com/libevent/libevent)
+
+ExternalProject_Add(  ${libevent_NAME}
+        PREFIX            ${BUILDEM_DIR}
+        GIT_REPOSITORY    ${libevent_URL}
+        GIT_TAG           ${libevent_TAG}
+        UPDATE_COMMAND    ${LIBEVENT_UPDATE_COMMAND}
+        PATCH_COMMAND     "./autogen.sh"
+        CONFIGURE_COMMAND ${BUILDEM_ENV_STR}
+            ./configure
+                --prefix=${BUILDEM_DIR}
+       BUILD_COMMAND     ${BUILDEM_ENV_STRING} $(MAKE)
+       BUILD_IN_SOURCE   1
+       TEST_COMMAND      ""
+       INSTALL_COMMAND   ${BUILDEM_ENV_STRING} $(MAKE) install
+)
+
+
