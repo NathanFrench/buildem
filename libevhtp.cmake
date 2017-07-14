33c6da11cbcfd4ff246b2c1026a4adb9134abf93
Nathan French <nate@cl0d.com>
2017-07-14 07:49:53 -0700

    I suck

--- libevhtp.cmake
+++ libevhtp.cmake
@@ -6,7 +6,7 @@ if (NOT libevhtp_NAME)
 
 external_git_repo (libevhtp
     HEAD
-    https://github.com/criticalstack/libevhtp) 
+    https://www.github.com/criticalstack/libevhtp) 
 
 ExternalProject_Add(${libevhtp_NAME}
         PREFIX            ${BUILDEM_DIR}
@@ -15,7 +15,7 @@ ExternalProject_Add(${libevhtp_NAME}
         UPDATE_COMMAND    "" 
         BUILD_IN_SOURCE 1
        TEST_COMMAND      ""
-     CONFIGURE_COMMAND ${BUILDEM_ENV_STR} ${CMAKE_COMMAND} ${nghttp2_SRC_DIR} 
+     CONFIGURE_COMMAND ${BUILDEM_ENV_STR} ${CMAKE_COMMAND} ${libevhtp_SRC_DIR} 
 		-DCMAKE_EXE_LINKER_FLAGS=-L${BUILDEM_LIB_DIR}
         -DCMAKE_INSTALL_PREFIX:PATH=${BUILDEM_DIR}
 )
